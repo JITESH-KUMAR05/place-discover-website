5f8d1d7dad4d145217a6bc1909feb40b2034f2dc
Jitesh kumar <super.user.hello.world@gmail.com>
2023-09-14 21:02:17 +0530

    first commit

---
+++ practice js/app.js
@@ -0,0 +1,11 @@
+let input=document.getElementById("input")
+let querry = "https://source.unsplash.com/1600x900/?"+input.value
+
+search= ()=>{
+    console.log("button clicked.....")
+    let querry = "https://source.unsplash.com/1600x900/?"+input.value
+    console.log(querry);
+}
+function renderclass(){
+    
+}
